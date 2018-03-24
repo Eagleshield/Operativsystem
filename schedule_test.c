0c2d1c5a77ccb2b10de46aa1438c03686eebca29
Felix Ödmark <c14fok@cs.umu.se>
2018-03-24 15:08:36 +0100

    hm

--- schedule_test.c
+++ schedule_test.c
@@ -9,7 +9,8 @@
 typedef void *(*func_ptr)(void*);
 
 void run_threads(func_ptr func, int num_threads);
-void *write_test(void *args);
+void *write_test_static(void *args);
+void *write_test_dynamic(void *arg);
 
 pthread_barrier_t barrier;
 
@@ -26,17 +27,20 @@ int main(void) {
     pthread_barrier_init(&barrier, NULL, num_threads);
     
     printf("%s\n", "Noop write-test...");
-    run_threads(&write_test, num_threads);
+    //run_threads(&write_test_static, num_threads);
+    run_threads(&write_test_dynamic, num_threads);
 
     system("echo deadline | sudo tee /sys/block/sda/queue/scheduler");
     system("cat /sys/block/sda/queue/scheduler");
     printf("%s\n", "Deadline write-test...");
-    run_threads(&write_test, num_threads);
+    //run_threads(&write_test_static, num_threads);
+    run_threads(&write_test_dynamic, num_threads);
 
 	system("echo cfq | sudo tee /sys/block/sda/queue/scheduler");
     system("cat /sys/block/sda/queue/scheduler");
     printf("%s\n", "Cfq write-test...");
-    run_threads(&write_test, num_threads);    
+    //run_threads(&write_test_static, num_threads);    
+    run_threads(&write_test_dynamic, num_threads);
     
     system("sudo hdparm -W 1 /dev/sda");
     return 0;
@@ -56,7 +60,7 @@ void run_threads(func_ptr func, int num_threads) {
     }
 }
 
-void *write_test(void *arg) {
+void *write_test_static(void *arg) {
     args *t_args = arg;
     size_t size = 1000000000;
     char *big_boy = malloc(size);
@@ -94,6 +98,68 @@ void *write_test(void *arg) {
     fwrite(big_boy, size, 1, fp);
 
     free(big_boy);
+    fclose(fp);
+
+    /* Timer end */
+    gettimeofday(&tval_after, NULL);
+    timersub(&tval_after, &tval_before, &tval_result);
+
+    printf("Time elapsed: %ld.%06ld\n", (long int)tval_result.tv_sec, (long int)tval_result.tv_usec);
+
+    return NULL;
+}
+
+void *write_test_dynamic(void *arg) {
+	args *t_args = arg;
+    size_t size[] = {1000000000, 5000000, 1000000};
+    char *big_boy = malloc(size[0]);
+    char *medium_boy = malloc(size[1]);
+    char *small_boy = malloc(size[2]);
+
+    for(int i = 0; i < size[0]; i++) {
+        big_boy[i] = 'X';
+        if(i < size[1]) {
+        	medium_boy[i] = 'Y';
+        	if(i < size[2]) {
+        		small_boy[i] = 'Z';
+        	}
+        }
+    }
+    struct timeval tval_before, tval_after, tval_result;
+    pthread_barrier_wait(&barrier);
+    
+    /* Timer start */
+    gettimeofday(&tval_before, NULL);
+
+    char file_name[9] = {'g','a','r','b','a','g','e','e'};
+    file_name[7] = t_args->tids;
+
+	FILE *fp;
+	if(t_args->tid%2 == 0) {
+        char *filep = malloc(32*sizeof(char));
+        strcpy(filep, "../testdirectory/");
+        strcat(filep, file_name);
+		fp = fopen(filep, "w");
+		//printf("%s\n", filep);
+    } else {
+		fp = fopen(file_name, "w");
+    	//printf("%s\n", file_name);
+	}
+
+    perror("fopen");
+    if(fp == NULL) {
+        fprintf(stderr, "%s\n", "File not created.");
+        return NULL;
+	}
+
+    fwrite(small_boy, size[2], 1, fp);
+    fwrite(medium_boy, size[1], 1, fp);
+    fwrite(big_boy, size[0], 1, fp);
+
+
+    free(big_boy);
+    free(medium_boy);
+    free(small_boy);
     fclose(fp);
 
     /* Timer end */
