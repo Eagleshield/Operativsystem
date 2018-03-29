283d5bdcb7e550504ef123ab67a3979e84dac9eb
Felix Ödmark <c14fok@cs.umu.se>
2018-03-29 17:51:56 +0200

    test repeat script

--- threaded.sh
+++ threaded.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
-#input 1 -> threads, input 2 -> time
 
-echo noop > /sys/block/sda/queue/scheduler
-cat /sys/block/sda/queue/scheduler
-
-bash write_test.sh
-bash read_test.sh
-
-echo deadline > /sys/block/sda/queue/scheduler
-cat /sys/block/sda/queue/scheduler
-
-bash write_test.sh
-bash read_test.sh
-
-echo cfq > /sys/block/sda/queue/scheduler
-cat /sys/block/sda/queue/scheduler
-
-bash write_test.sh
-bash read_test.sh
+for (( i = 0; i < 30; i++ )); do
+	./sched_test
+done
