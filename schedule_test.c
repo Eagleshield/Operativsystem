f4f44ca5d83b0ffdc096521e5dcc74761268deda
Felix Ödmark <c14fok@cs.umu.se>
2018-03-29 15:03:46 +0200

    hm

--- schedule_test.c
+++ schedule_test.c
@@ -27,7 +27,7 @@ int main(void) {
     system("echo noop | sudo tee /sys/block/sda/queue/scheduler");
     system("cat /sys/block/sda/queue/scheduler");
     system("sudo hdparm -W 0 /dev/sda");
-    int num_threads = 5;
+    int num_threads = 6;
     pthread_barrier_init(&barrier, NULL, num_threads);
     struct timeval tval_before, tval_after, tval_result;
 
@@ -182,7 +182,7 @@ void *write_test_dynamic(void *arg) {
 
 	if(t_args->tid == 0)
 		printf("%s\n", "Dynamic size test");
-	
+
     struct timeval tval_before, tval_after, tval_result;
     pthread_barrier_wait(&barrier);
     
