56d9c27999f4fd588cc0cb8b2804c1cbb5959300
Felix Ödmark <c14fok@cs.umu.se>
2018-06-05 15:58:18 +0200

    hm

--- test_script.sh
+++ test_script.sh
@@ -2,8 +2,15 @@
 
 SCHEDLIST='cfq deadline noop'
 
+sudo hdparm -W 0 /dev/sda
+
 for sched in $SCHEDLIST; do
+	echo 3 > sudo tee /proc/sys/vm/drop_caches
+
 	echo $sched | sudo tee /sys/block/sda/queue/scheduler
 	cat /sys/block/sda/queue/scheduler
+
+
 done
 
+sudo hdparm -W 1 /dev/sda
