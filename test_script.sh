9654430110922e6d21e495a38686dd8e4b7ed7cd
Felix Ödmark <c14fok@cs.umu.se>
2018-06-05 15:56:05 +0200

    hm

--- test_script.sh
+++ test_script.sh
@@ -4,6 +4,6 @@ SCHEDLIST='cfq deadline noop'
 
 for sched in $SCHEDLIST; do
 	echo $sched | sudo tee /sys/block/sda/queue/scheduler
-	echo cat /sys/block/sda/queue/sheduler
+	cat /sys/block/sda/queue/scheduler
 done
 
